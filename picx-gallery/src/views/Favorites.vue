89e9dc5ee3716efb57273223b461a015cf1a0a95
soymariavillaverde <soymariavillaverde@gmail.com>
2022-09-19 16:49:50 +0200

    Set up basics for favorites page/ TO DO the layout :construction:

--- picx-gallery/src/views/Favorites.vue
+++ picx-gallery/src/views/Favorites.vue
@@ -1,3 +1,9 @@
 <template>
-<div>página favoritos</div>
+    <v-container fluid>
+        <v-row wrap>
+            <div class="favsContainer">
+                <h1>We're sorry, this page is under construction</h1>
+            </div>
+        </v-row>
+    </v-container>
 </template>
